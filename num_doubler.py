ea74dce119a12a90e16445d6886726188453d3ef
bloneybloney <bloney@champlaincollege.qc.ca>
2022-07-14 02:15:12 +0000

    My first commit

---
+++ num_doubler.py
@@ -0,0 +1,8 @@
+def doubler(number):
+    result = number * 2
+    return result
+
+get_number = float(input("Type a number:  "))
+print("Getting your answer....")
+answer = doubler(get_number)
+print(answer)
